8f52f2a573e1c92baf28cf73edae5ac0de584e51
antran77 <antran.bc@gmail.com>
2020-05-08 10:48:41 +0700

    + move player

---
+++ RollABall/Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public Rigidbody m_rigibody;
+
+    void Start()
+    {
+        
+
+         
+    }
+
+    void FixedUpdate() {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+        m_rigibody.AddForce(movement * speed);
+    }
+
+    
+}
